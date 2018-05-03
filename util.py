880953a22d0a97213bf3c71c4710b67c0d830d7c
tomHuang <tom.huang@positivegrid.com>
2018-05-03 14:10:35 +0800

    chore: loosen restriction
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- util.py
+++ util.py
@@ -20,7 +20,7 @@ def load_data(path, valid_split=0.1):
     raw_datum = [ float(val) for val in line.split(', ')[:-4] ]
     xyz = [float(val) for val in line.split(', ')[-4:-1]]
     raw_datum = [raw_datum[i] for i in range(len(raw_datum)) if i % 3 <= 1] # ignore noise level
-    if (np.count_nonzero(raw_datum) >= 72):
+    if (np.count_nonzero(raw_datum) >= 68):
       raw_data.append(raw_datum)
       if (not np.array_equal(curr_xyz, xyz)):
         curr_xyz = xyz
@@ -28,7 +28,7 @@ def load_data(path, valid_split=0.1):
         label_to_xyz.append(xyz)
       labels.append(curr_label)
   raw_data = np.array(raw_data)
-  selected_features = np.sort(np.argsort(np.count_nonzero(raw_data, axis=0))[15:])
+  selected_features = np.sort(np.argsort(np.count_nonzero(raw_data, axis=0))[18:])
   print(selected_features)
   raw_data = raw_data[:,selected_features]
   raw_data /= 100
@@ -49,7 +49,7 @@ def transform_feat(raw_feat, selected_features):
   feat = raw_feat
   sig_feat_idxs = [i for i in range(raw_feat.shape[1]) if i % 3 <= 1]
   feat = feat[:,sig_feat_idxs]
-  qualified_data_idxs = [j for j in range(raw_feat.shape[0]) if np.count_nonzero(feat[j,:]) >= 72]
+  qualified_data_idxs = [j for j in range(raw_feat.shape[0]) if np.count_nonzero(feat[j,:]) >= 68]
   feat = feat[qualified_data_idxs, :]
   feat = feat[:,selected_features]
   feat /= 100
